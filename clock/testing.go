0f864b7558414372a01fef14da04ade402fe5c91
Matthias Endler <matthias.endler@endlernet.com>
2021-11-16 01:31:02 +0100

    add runing clock for testing

--- clock/testing.go
+++ clock/testing.go
@@ -11,3 +11,13 @@ func StoppedAt(t time.Time) Func {
 		return t
 	}
 }
+
+// StartedAt is a function which returns a Func, which starts the clock at
+// the given time.Time. This is useful for testing, when you need a running
+// clock, but you want to control, when the clock started.
+func StartedAt(t time.Time) Func {
+	offset := t.Sub(time.Now())
+	return func() time.Time {
+		return time.Now().Add(offset)
+	}
+}
