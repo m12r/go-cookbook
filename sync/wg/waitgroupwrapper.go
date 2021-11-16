cbc91a2af5a075ee6960e554e5bcfeb88112a201
Matthias Endler <matthias.endler@endlernet.com>
2021-11-16 01:48:44 +0100

    add waitgroup wrapper

---
+++ sync/wg/waitgroupwrapper.go
@@ -0,0 +1,15 @@
+package wg
+
+import "sync"
+
+type WaitGroupWrapper struct {
+	sync.WaitGroup
+}
+
+func (w *WaitGroupWrapper) Wrap(cb func()) {
+	w.Add(1)
+	go func() {
+		defer w.Done()
+		cb()
+	}()
+}
